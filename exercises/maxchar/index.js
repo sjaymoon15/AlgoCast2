49aa6fa9f7658020aa5dea6b763a982419d6b959
Seungjae Moon <sjaymoon15@gmail.com>
2018-05-16 11:54:08 -0700

    Add a solution to maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let count = {};
+  for (let elem of str) {
+    if (count[elem]) {
+      count[elem]++;
+    } else {
+      count[elem] = 1;
+    }
+  }
+
+  let currentMax = 0;
+  let maxKey = '';
+  for (let key in count) {
+      if (currentMax < count[key]) {
+        currentMax = count[key];
+        maxKey = key;
+      }
+  }
+
+  return maxKey;
+}
 
 module.exports = maxChar;
--- exercises/maxchar/test.js
+++ exercises/maxchar/test.js
@@ -12,3 +12,7 @@ test('Finds the most frequently used char', () => {
 test('Works with numbers in the string', () => {
   expect(maxChar('ab1c1d1e1f1g1')).toEqual('1');
 });
+
+test('Works with spaces in the string', () => {
+  expect(maxChar('ab 1c d e f g')).toEqual(' ');
+});
