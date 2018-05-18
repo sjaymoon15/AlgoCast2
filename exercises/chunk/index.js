6acc7bc8bd09a443e6ca1bba3e0a90b2023cc691
Seungjae Moon <sjaymoon15@gmail.com>
2018-05-18 11:35:54 -0700

    Add a solution to chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,25 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  if (size === 0 || size >= array.length) {
+    return array;
+  }
+  let chunked = [];
+  let smallChunk = [];
+  for (let i of array) {
+    if (smallChunk.length < size) {
+      smallChunk.push(i);
+    } else if (smallChunk.length === size) {
+      chunked.push(smallChunk);
+      smallChunk = [];
+    }
+  }
+
+  if (smallChunk.length > 0) {
+    chunked.push(smallChunk);
+  }
+  return chunked;
+}
 
 module.exports = chunk;
