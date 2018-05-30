589f97321f883e41d0eea7ad9582f7e928b60f48
Seungjae Moon <sjaymoon15@gmail.com>
2018-05-30 11:37:12 -0700

    Add my solution to steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -8,15 +8,34 @@
 //       '# '
 //       '##'
 //   steps(3)
-//       '#  '
-//       '## '
-//       '###'
+//         123
+//    1   '#  '
+//    2   '## '
+//    3   '###'
 //   steps(4)
 //       '#   '
 //       '##  '
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  // nested for loop. outer for looping through row
+  // inner loop for each row. colums
+
+  // keep track of indexes columnIndex and rowIndex
+  // inner loop should make a string and console log at the end
+
+  for(let row = 1; row <= n; row++) {
+    let step = '';
+    for(let column = 1; column <= n; column++) {
+      if (column <= row) {
+        step += '#';
+      } else {
+        step += ' ';
+      }
+    }
+    console.log(step);
+  }
+}
 
 module.exports = steps;
