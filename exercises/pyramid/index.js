c31a9cf4bb457a80a13d13c7e90637f973ab013f
Seungjae Moon <sjaymoon15@gmail.com>
2018-05-31 15:18:43 -0700

    Add a solution to pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -5,15 +5,42 @@
 // pyramid has spaces on both the left *and* right hand sides
 // --- Examples
 //   pyramid(1)
-//       '#'
+//        1
+//       '#'1
 //   pyramid(2)
-//       ' # '
-//       '###'
+//        123
+//       ' # '1
+//       '###'2
 //   pyramid(3)
-//       '  #  '
-//       ' ### '
-//       '#####'
+//        12345
+//       '  #  '1
+//       ' ### '2
+//       '#####'3
+//   pyramid(4)
+//       1234567
+//      '   #   '1
+//      '  ###  '2
+//      ' ##### '3
+//      '#######'4
 
-function pyramid(n) {}
+function pyramid(n) {
+  // 1, 3, 5, 7
+  // 2n - 1
+  // for n row there will be 2n - 1 columns
+
+  const columnWidth = 2 * n - 1;
+
+  for(let row = 1; row <= n; row++) {
+    let newRow = '';
+    for(let column = 1; column <= columnWidth; column++) {
+      if (column >= n - (row - 1) && column <= n + (row - 1)) {
+        newRow += '#';
+      } else {
+        newRow += ' ';
+      }
+    }
+    console.log(newRow);
+  }
+}
 
 module.exports = pyramid;
